3214f8764347352c1460dd1e807944e2da341f94
Prof. Hugo - SENAC <hugo.tannus@go.senac.br>
2024-09-03 14:37:45 -0300

    Substitui funções por importações da biblioteca `csv.js`

--- showCSV.js
+++ showCSV.js
@@ -1,6 +1,9 @@
 //// Função Principal
 const fs = require('fs');
 const csv = fs.readFileSync('./sample.csv').toString();
+const {
+    quebraLinhas, separaColunas, separaConteudo, constroiListaDeObjetos
+} = require('./csv');
 
 const linhas = quebraLinhas(csv);
 const primeiraLinha = linhas.shift();
@@ -14,52 +17,3 @@ const json = constroiListaDeObjetos(headers, conteudo);
 console.table(json, ["Filme", "Ano", "Gênero"]);
 
 /////////////////////////////
-
-function quebraLinhas(csv) {
-    let linhas = csv.trim().split('\n');
-    return linhas;
-}
-
-function separaColunas(linha) {
-    let colunas = linha.trim().split(';')
-    return colunas;
-}
-
-function separaConteudo(lines) {
-    const conteudo = [];
-
-    for(let i=0; i<lines.length; i++) {
-        let linha = lines[i];
-        let colunas = separaColunas(linha);
-
-        conteudo.push(colunas);
-    }
-
-    return conteudo;
-}
-
-function constroiObjeto(headers, linhaTabela) {
-    let obj = {};
-
-    for(let j=0; j<headers.length; j++){
-        let titulo = headers[j];
-        let coluna = linhaTabela[j];
-
-        obj[titulo] = coluna;
-    }
-
-    return obj;
-}
-
-function constroiListaDeObjetos(headers, conteudo) {
-    const listaDeObjetos = [];
-
-    for(let i=0; i<conteudo.length; i++) {
-        const linhaTabela = conteudo[i];
-        const obj = constroiObjeto(headers,linhaTabela);
-
-        listaDeObjetos.push(obj);
-    }
-
-    return listaDeObjetos;
-}
